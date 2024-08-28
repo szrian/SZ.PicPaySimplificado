f9322782b66bc99606723e6b18b3d26fad8fd392
Rian de Souza Araújo <100230492+szrian@users.noreply.github.com>
2024-08-27 22:59:40 -0300

    feat: configuracao banco de dados

---
+++ src/SZ.PicPaySimplificado.Dados/Contexto/PicPaySimplificadoContexto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SZ.PicPaySimplificado.Dados.Contexto
+{
+	public class PicPaySimplificadoContexto : DbContext
+	{
+		public PicPaySimplificadoContexto(DbContextOptions<PicPaySimplificadoContexto> options) : base(options)
+		{ }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(PicPaySimplificadoContexto).Assembly);
+			base.OnModelCreating(modelBuilder);
+
+			foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.EnableSensitiveDataLogging();
+		}
+	}
+}
---
+++ src/SZ.PicPaySimplificado.Dados/Modulos/InstalarDependencias.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SZ.PicPaySimplificado.Dados.Contexto;
+
+namespace SZ.PicPaySimplificado.Dados.Modulos
+{
+	public static class InstalarDependencias
+	{
+		public static IServiceCollection AdicionarBancoDeDados(this IServiceCollection servicos, IConfiguration configuracao) =>
+			servicos.AddDbContext<PicPaySimplificadoContexto>(opcoes => opcoes.UseSqlServer(configuracao.GetConnectionString("DefaultConnection")));
+	}
+}

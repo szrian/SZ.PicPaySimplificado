31e89d29368b230a3aca65149a615d1b1c8efb96
Rian de Souza Araújo <100230492+szrian@users.noreply.github.com>
2024-08-28 23:59:10 -0300

    feat: adicionar migration inicial

---
+++ src/SZ.PicPaySimplificado.Dados/Migrations/20240829025533_migracaoInicial.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SZ.PicPaySimplificado.Dados.Migrations
+{
+    /// <inheritdoc />
+    public partial class migracaoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Documento = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false),
+                    TipoUsuario = table.Column<int>(type: "int", nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transacoes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Valor = table.Column<float>(type: "real", nullable: false),
+                    PagadorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RecebedorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transacoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transacoes_Usuarios_PagadorId",
+                        column: x => x.PagadorId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Transacoes_Usuarios_RecebedorId",
+                        column: x => x.RecebedorId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacoes_PagadorId",
+                table: "Transacoes",
+                column: "PagadorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacoes_RecebedorId",
+                table: "Transacoes",
+                column: "RecebedorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_Documento",
+                table: "Usuarios",
+                column: "Documento",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_Email",
+                table: "Usuarios",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transacoes");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}

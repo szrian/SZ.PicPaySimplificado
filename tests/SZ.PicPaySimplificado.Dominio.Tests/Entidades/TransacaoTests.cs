2eb71f2c5b47449df44463d2e18fb297ed5368ea
Rian Araujo <rian_s_araujo@hotmail.com>
2025-07-29 22:46:09 -0300

    feat: testes unitarios da entidade transacao

---
+++ tests/SZ.PicPaySimplificado.Dominio.Tests/Entidades/TransacaoTests.cs
@@ -0,0 +1,56 @@
+﻿using Bogus;
+using FluentAssertions;
+using SZ.PicPaySimplificado.Dominio.Modelos;
+using SZ.PicPaySimplificado.Dominio.Tests.Fakers;
+
+namespace SZ.PicPaySimplificado.Dominio.Tests.Entidades;
+
+public class TransacaoTests
+{
+    private readonly Faker _faker = new("pt_BR");
+
+    [Fact]
+    public void Construtor_DadoOsParametros_DeveSetarAsPropriedadesCorretamente()
+    {
+        //Arrange
+        var valorEsperado = _faker.Random.Float(1,100);
+        var pagadorIdEsperado = Guid.NewGuid();
+        var recebedorIdEsperado = Guid.NewGuid();
+
+        //Act
+        var transacao = new Transacao(valorEsperado,
+            pagadorIdEsperado,
+            recebedorIdEsperado);
+
+        //Assert
+        transacao.Valor.Should().Be(valorEsperado);
+        transacao.PagadorId.Should().Be(pagadorIdEsperado);
+        transacao.RecebedorId.Should().Be(recebedorIdEsperado);
+    }
+
+    [Fact]
+    public void Validar_DadaTransacaoValida_DeveRetornarValidationResultTrue()
+    {
+        //Arrange
+        var transacao = TransacaoFaker.GerarTransacaoValida();
+
+        //Act
+        transacao.Validar();
+
+        //Assert
+        transacao.ValidationResult.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Validar_DadaTransacaoInvalida_DeveRetornarValidationResultFalse()
+    {
+        //Arrange
+        var transacao = TransacaoFaker.GerarTransacaoInvalida();
+
+        //Act
+        transacao.Validar();
+
+        //Assert
+        transacao.ValidationResult.IsValid.Should().BeFalse();
+    }
+}
---
+++ tests/SZ.PicPaySimplificado.Dominio.Tests/Fakers/TransacaoFaker.cs
@@ -0,0 +1,23 @@
+﻿using Bogus;
+using SZ.PicPaySimplificado.Dominio.Modelos;
+
+namespace SZ.PicPaySimplificado.Dominio.Tests.Fakers;
+
+public static class TransacaoFaker
+{
+    public static Transacao GerarTransacaoValida()
+    {
+        var faker = new Faker("pt_BR");
+        return new Transacao(faker.Random.Float(1, 100),
+            Guid.NewGuid(),
+            Guid.NewGuid());
+    }
+
+    public static Transacao GerarTransacaoInvalida()
+    {
+        var faker = new Faker("pt_BR");
+        return new Transacao(0,
+            Guid.Empty,
+            Guid.Empty);
+    }
+}

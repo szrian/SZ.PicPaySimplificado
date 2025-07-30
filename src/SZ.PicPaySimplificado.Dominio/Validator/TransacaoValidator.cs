f923e5e217d55afc4ec9b5e286bae24c0cd13b11
Rian Araujo <rian_s_araujo@hotmail.com>
2025-07-29 22:45:13 -0300

    fix: corrigir validacao da entidade transacao

--- src/SZ.PicPaySimplificado.Dominio/Validator/TransacaoValidator.cs
+++ src/SZ.PicPaySimplificado.Dominio/Validator/TransacaoValidator.cs
@@ -8,12 +8,13 @@ public class TransacaoValidator : AbstractValidator<Transacao>
     public TransacaoValidator()
     {
         RuleFor(p => p.Valor)
-            .NotNull().WithMessage("Valor da transação não informado.");
+            .NotNull().WithMessage("Valor da transação não informado.")
+			.GreaterThan(0).WithMessage("Valor informado deve ser maior que 0.");
 
 		RuleFor(p => p.PagadorId)
-			.NotNull().WithMessage("Usuário pagador não informado.");
+			.NotEmpty().WithMessage("Usuário pagador não informado.");
 
 		RuleFor(p => p.PagadorId)
-			.NotNull().WithMessage("Usuário recebedor não informado.");
+			.NotEmpty().WithMessage("Usuário recebedor não informado.");
 	}
 }
